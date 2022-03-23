0e8ebdee31b158e2d24762bd35e9955ef8bb9265
Chris <bluman325@gmail.com>
2022-03-23 14:58:53 -0500

    added tasks to RPSLS file

---
+++ RPSLS.py
@@ -0,0 +1,19 @@
+#algorithm
+#1 display a welcome
+#2 Display rules of game
+#3 single player or multiplayer (user input)
+#4 choose first player to go
+#5 player who goes first chooses a gesture
+#6 second player chooses a gesture
+#7 compare the gestures
+#8 based off the rules, decide who won
+#9 keep track of score
+#10 repeat steps 4-9
+#11 whoever gets the best of three first is the winner
+#12 display the winner
+
+#classes
+#1 player (parent)
+#2 Game
+#3 human (child) >inherits from player
+#4 AI (child) > inherits from player
